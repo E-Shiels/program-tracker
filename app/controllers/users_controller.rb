aeb7514d9c993842d9a919168bcc2c79eb57b327
Eric Shiels <ericshiels2@gmail.com>
2018-10-10 17:10:16 -0400

    Adding blank routes to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
 
+get '/login' do
+
+end
+
+post '/login' do
+
+end
+
+get '/signup' do
+
+end
+
+post '/signup' do
+
+end
+
+get '/logout' do
+
+end
+
+
 end
