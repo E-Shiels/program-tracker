86c3a51b2f70a4d8a95df462a31d74cb75cce394
Eric Shiels <ericshiels2@gmail.com>
2018-10-11 17:50:48 +0000

    working on programs and users controller

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,15 +2,17 @@ class ProgramsController < ApplicationController
 
 
   get '/programs' do
-
+    @programs = Program.all
+    erb :'programs/index'
   end
 
   get '/programs/new' do
-
+    erb :'programs/new'
   end
 
   get '/programs/:id' do
-
+    @program = Program.find(params[:id])
+    erb :'programs/edit'
   end
 
   get '/programs/:id/edit' do
