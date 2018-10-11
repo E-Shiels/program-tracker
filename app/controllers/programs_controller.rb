12ffd19d6663eed1d43501f36b7cadeeb3e01ac3
Eric Shiels <ericshiels2@gmail.com>
2018-10-11 21:06:15 +0000

    first finished version of all routes, changed from buttons to link on placeholder index page, other minor changes

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -12,7 +12,7 @@ class ProgramsController < ApplicationController
 
   get '/programs/:id' do
     @program = Program.find(params[:id])
-    erb :'programs/edit'
+    erb :'programs/show'
   end
 
   get '/programs/:id/edit' do
@@ -21,15 +21,22 @@ class ProgramsController < ApplicationController
   end
 
   post '/programs' do
-
+    @program = Program.create(params['program'])
+    @program.save
+    redirect '/programs/#{@program.id}'
   end
 
   patch '/programs/:id' do
-
+    @program = Program.find_by_id(params[:id])
+    @program.update(params[:program])
+    @program.save
+    redirect to '/programs/#{@program.id}'
   end
 
   delete '/programs/:id/delete' do
-
+    @program = Program.find_by_id(params[:id])
+    @program.destroy
+    redirect '/programs'
   end
 
 end
