68662f8dc0a4db6d038b1398ddeab5dccca2529a
Eric Shiels <ericshiels2@gmail.com>
2018-10-12 18:48:09 +0000

    adding categories to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
   User.create(password_digest: 'abcdefg', email: 'a@example.com')
+  User.create(password_digest: '123456789', email: '1@123.com')
+
   Program.create(name: 'program name', description: 'it is a program', url: "www.google.com", install_date: "Tuesday, October 11 2018", user_id: "1")
+  Program.create(name: 'program name', description: 'it is a program', url: "www.google.com", install_date: "Tuesday, October 11 2018", user_id: "1")
+
+  Category.create(name: 'utilities')
+  Category.create(name: 'games')
+  Category.create(name: 'photo and video')
+  
