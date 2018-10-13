5adb5bcff36b5130c91ebdc48a0f3952df6b9a6b
Eric Shiels <ericshiels2@gmail.com>
2018-10-13 19:02:19 +0000

    programs/new working until I add categoroes

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,30 +1,36 @@
 class ProgramsController < ApplicationController
 
   get '/programs' do
+    redirect_if_not_logged_in
     @programs = Program.all
     erb :'programs/index'
   end
 
   get '/programs/new' do
+    redirect_if_not_logged_in
     erb :'programs/new'
   end
 
   post '/programs' do
-    @program = Program.create(params['program'])
-    redirect '/programs/:id'
+    redirect_if_not_logged_in
+    @program = Program.create(params)
+    redirect '/programs'
   end
 
   get '/programs/:id' do
+    redirect_if_not_logged_in
     @program = Program.find(params[:id])
     erb :'programs/show'
   end
 
   get '/programs/:id/edit' do
+    redirect_if_not_logged_in
     @program = Program.find(params[:id])
     erb :'programs/edit'
   end
 
   patch '/programs/:id' do
+    redirect_if_not_logged_in
     @program = Program.find_by_id(params[:id])
     @program.update(params[:program])
     @program.save
