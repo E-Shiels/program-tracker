79d274ad96414e93d5a311f3acda6b3fffa48ed1
Eric Shiels <ericshiels2@gmail.com>
2018-10-15 19:15:15 +0000

    improving seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-  User.create(password_digest: 'abcdefg', email: 'a@example.com')
-  User.create(password_digest: '123456789', email: '1@123.com')
+  User.create(password: 'abcdefg', email: 'a@example.com', id: "1")
+  User.create(password: '123456789', email: '1@123.com', id: "2")
+  User.create(password: 'qwerty', email: 'mike@microsoft.com', id: "3")
+  User.create(password: 'asdfgh', email: 'larry@gmail.com', id: "4")
 
-  Program.create(name: 'program name', description: 'it is a program', url: "www.google.com", install_date: "Tuesday, October 11 2018", user_id: "1")
-  Program.create(name: 'program name', description: 'it is a program', url: "www.google.com", install_date: "Tuesday, October 11 2018", user_id: "4")
 
-  Category.create(name: 'utilities')
-  Category.create(name: 'games')
-  Category.create(name: 'photo and video')
+  Program.create(name: 'program name', description: 'it is a program', url: "install.com", install_date: "October 11 2018", user_id: "1")
+  Program.create(name: 'good program', description: 'it is a good program', url: "setup.ca", install_date: "October 12 2018", user_id: "1")
+  Program.create(name: 'great program', description: 'it is a great program', url: "download.co", install_date: "October 13 2018", user_id: "1")
+  Program.create(name: 'bad program', description: 'it is a bad program', url: "test.co.uk", install_date: "October 14 2018", user_id: "1")
+
+  Program.create(name: 'program name', description: 'it is a program', url: "install.com", install_date: "October 11 2018", user_id: "2")
+  Program.create(name: 'good program', description: 'it is a good program', url: "setup.ca", install_date: "October 12 2018", user_id: "2")
+  Program.create(name: 'great program', description: 'it is a great program', url: "download.co", install_date: "October 13 2018", user_id: "2")
+  Program.create(name: 'bad program', description: 'it is a bad program', url: "test.co.uk", install_date: "October 14 2018", user_id: "2")
+
+  Program.create(name: 'program name', description: 'it is a program', url: "install.com", install_date: "October 11 2018", user_id: "3")
+  Program.create(name: 'good program', description: 'it is a good program', url: "setup.ca", install_date: "October 12 2018", user_id: "3")
+  Program.create(name: 'great program', description: 'it is a great program', url: "download.co", install_date: "October 13 2018", user_id: "3")
+  Program.create(name: 'bad program', description: 'it is a bad program', url: "test.co.uk", install_date: "October 14 2018", user_id: "3")
+
+  Program.create(name: 'program name', description: 'it is a program', url: "install.com", install_date: "October 11 2018", user_id: "4")
+  Program.create(name: 'good program', description: 'it is a good program', url: "setup.ca", install_date: "October 12 2018", user_id: "4")
+  Program.create(name: 'great program', description: 'it is a great program', url: "download.co", install_date: "October 13 2018", user_id: "4")
+  Program.create(name: 'bad program', description: 'it is a bad program', url: "test.co.uk", install_date: "October 14 2018", user_id: "4")
