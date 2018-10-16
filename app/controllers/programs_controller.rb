77aa812982077058d73f22928329043e3dc5e515
Eric Shiels <ericshiels2@gmail.com>
2018-10-16 20:36:25 +0000

    adding capitalization and fixing date parsing in new program

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -13,8 +13,12 @@ class ProgramsController < ApplicationController
 
   post '/programs' do
     redirect_if_not_logged_in
-    @program = Program.create(params)
-    @program.install_date = Chronic.parse(@program.install_date).to_datetime.strftime("%F")
+    @program = Program.new
+    @program.name = params[:name]
+    @program.category = params[:category].slice(0,1).capitalize + params[:category].slice(1..-1)
+    @program.description = params[:description].slice(0,1).capitalize + params[:description].slice(1..-1)
+    @program.url = params[:url]
+    @program.install_date = Chronic.parse(params[:install_date]).to_datetime.strftime("%F")
     @program.user_id = current_user.id
     @program.save
     redirect to "/programs/#{@program.id}"
