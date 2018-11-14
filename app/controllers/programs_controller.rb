bf5f7af34ab43a70b27c4c8ac2175b5986009eb9
Eric Shiels <ericshiels2@gmail.com>
2018-11-14 21:37:51 +0000

    refactoring controllers with notes from meeting

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,7 +2,7 @@ class ProgramsController < ApplicationController
 
   get '/programs' do
     redirect_if_not_logged_in
-    @programs = Program.all.select {|program| program.user_id == current_user.id}
+    @programs = current_user.programs
     erb :'programs/index'
   end
 
@@ -13,25 +13,29 @@ class ProgramsController < ApplicationController
 
   post '/programs' do
     redirect_if_not_logged_in
-    @program = Program.new
-    @program.name = params[:name]
-    if params[:category].nil? || params[:category].blank? || params[:category].empty?
-      @program.category = params[:category]
-    else
-      @program.category = params[:category].slice(0,1).capitalize + params[:category].slice(1..-1)
-    end
-    if params[:description].nil? || params[:description].blank? || params[:description].empty?
-      @program.description = params[:description]
-    else
-      @program.description = params[:description].slice(0,1).capitalize + params[:description].slice(1..-1)
-    end
-    @program.url = params[:url]
+    @program = Program.new(:name => params[:name], :category => params[:category].capitalize, :description => params[:description].capitalize, :url => params[:url], :user_id => current_user.id)
+
+  #  @program.name = params[:name]
+    # if params[:category].nil? || params[:category].blank? || params[:category].empty?
+    #   @program.category = params[:category]
+    # else
+      # @program.category = params[:category].capitalize
+    # end
+    # if params[:description].nil? || params[:description].blank? || params[:description].empty?
+    #   @program.description = params[:description]
+    # else
+    #   @program.description = params[:description].capitalize
+    # end
+  #  @program.url = params[:url]
+
     if Chronic.parse(params[:install_date]).nil?
       @program.install_date = params[:install_date]
     else
       @program.install_date = Chronic.parse(params[:install_date]).to_datetime.strftime("%B %d, %Y")
     end
-    @program.user_id = current_user.id
+
+  #  @program.user_id = current_user.id
+
     @program.save
     redirect to "/programs/#{@program.id}"
   end
@@ -63,7 +67,6 @@ class ProgramsController < ApplicationController
     @program = Program.find_by_id(params[:id])
     if @program.user_id == current_user.id
       @program.update(params)
-      @program.save
       redirect to "/programs/#{@program.id}"
     else
       redirect to "/programs"
